f2b6483431e4be9eee5dbe1d18b80d8f9fdb35bd
Taylor Bird <nsubordin81@gmail.com>
2016-12-04 17:35:17 -0500

    hiding instance variables

--- checkout_machine.rb
+++ checkout_machine.rb
@@ -1,42 +1,43 @@
 class CheckoutMachine
-  def initialize
-    @balance = 0
-    @bonus_card_scanned = false
-    @salsa_counter = 0
-    @chip_counter = 0
+  attr_accessor :balance, :bonus_card_scanned, :salsa_counter, :chip_counter
+  def initialize(balance = 0, bonus_card_scanned = false, salsa_counter = 0, chip_counter = 0)
+    @balance = balance
+    @bonus_card_scanned = bonus_card_scanned
+    @salsa_counter = salsa_counter
+    @chip_counter = chip_counter
   end
 
   def scan(sku)
     update_balance(sku)
 
-    @bonus_card_scanned = true if sku == 000
+    self.bonus_card_scanned = true if sku == 000
   end
 
   def total
     apply_discount
-    @balance
+    self.balance
   end
 
   private
 
   def apply_discount
-    if @bonus_card_scanned
-      @balance -= 50 * @salsa_counter
-      @balance -= 200 * (@chip_counter/3).floor
+    if self.bonus_card_scanned
+      self.balance -= 50 * self.salsa_counter
+      self.balance -= 200 * (@chip_counter/3).floor
     end
   end
 
   def update_balance(sku)
     if sku == 123
-      @chip_counter += 1
-      @balance += 200
+      self.chip_counter += 1
+      self.balance += 200
     elsif sku == 456
-      @salsa_counter += 1
-      @balance += 100
+      self.salsa_counter += 1
+      self.balance += 100
     elsif sku == 789
-      @balance += 1000
+      self.balance += 1000
     elsif sku == 111
-      @balance += 550
+      self.balance += 550
     end
   end
 end
